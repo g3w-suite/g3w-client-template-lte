d717cedc2231c0b93f7e11691ab65705ec289bd8
volterra79 <francescoboccacci@libero.it>
2019-06-12 14:54:48 +0200

    added logout text in i18n and addes some css ruls for right tooltip

--- config/i18n/index.js
+++ config/i18n/index.js
@@ -2,13 +2,15 @@ export default {
   "it": {
     "template": {
       g3wSuiteFramework: "Applicativo realizzato con il framework OS",
-      productOf: "Prodotto da"
+      productOf: "Prodotto da",
+      logout: "Esci"
     }
   },
   "en": {
     "template": {
       g3wSuiteFramework: "Application based on OS framework",
-      productOf: "A product of"
+      productOf: "A product of",
+      logout: "Logout"
     }
   }
 }
