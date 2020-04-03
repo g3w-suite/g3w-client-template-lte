97cb97a37db04731fa6e7b5bbe4d34b89fce17da
volterra79 <francescoboccacci@libero.it>
2020-04-03 16:09:51 +0200

    moved style inlin onlistatus in less file

--- components/vue/OnlineStatus.vue
+++ components/vue/OnlineStatus.vue
@@ -1,11 +1,7 @@
 <template functional>
   <div id="offline" data-placement="right" v-if="!online"
        title="OFFLINE"
-       class="skin-tooltip-right"
-       style="position:absolute;
-       top: 55px; background-color: rgba(255,255,255,0.9);
-       padding: 5px; border-radius: 3px; color :red; left: 0;
-        z-index: 100; font-size: 25px;">
+       class="skin-tooltip-right">
     <i :class="g3wtemplate.getFontClass('wifi')"></i>
   </div>
 </template>
